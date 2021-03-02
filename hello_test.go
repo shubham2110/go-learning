c16782188e7aab5bbc825c6e56c1e63c8872d62c
shubham <you@aa.cc>
2021-03-02 03:17:05 +0000

    refraction done

--- hello_test.go
+++ hello_test.go
@@ -1,6 +1,58 @@
 package main
 
 import "testing"
+
+
+func TestHello(t *testing.T){
+
+	assertCorrectMessage := func(t testing.TB, got , want string){
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got , want)
+		}
+	}
+	
+	t.Run("saying hello to people", func(t *testing.T){
+		got := Hello("Shubham")
+		want := "Hello, Shubham"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to world", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+
+
+/*
+func TestHello(t *testing.T){
+t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("Chris")
+        want := "Hello, Chris"
+		
+		
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    })
+
+t.Run("saying hello to world", func(t *testing.T){
+	got := Hello("")
+	want := "Hello, World"
+	
+	if got != want {
+	
+		t.Errorf("got %q want %q", got , want)
+	
+	}
+})
+}
+*/
+
 /*
 func TestHello(t *testing.T) {
     got := Hello("Shubham")
@@ -16,15 +68,21 @@ func TestHello(t *testing.T) {
 	
 }
 */
-
+/*
 func TestHello(t *testing.T) {
-	got := Hello("World")
-	want := "Hello, World"
+	got := Hello("Shubham")
+	want := "Hello, Shubham"
 	
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	
 	}
-
-
+	
+	got = Hello("")
+	want = "Hello, World"
+	
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
+*/
