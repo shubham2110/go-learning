ad634d6eef7c357057da732debc4df2aa1bf8060
shubham2110 <saxena.shubham.2110@gmail.com>
2021-04-16 14:56:51 +0530

    Intriduction of running multiple tests in a single TestSum function

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -5,12 +5,27 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
 
-	if got != want {
-		t.Errorf("want %d got %d  given %v", want, got, numbers)
-	}
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
 }
