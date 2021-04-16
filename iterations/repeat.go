432be3ec543f921785ca737f40ccc761bf346bfa
shubham2110 <saxena.shubham.2110@gmail.com>
2021-04-16 14:22:45 +0530

    Aded exmaple and parameter to repeat

--- iterations/repeat.go
+++ iterations/repeat.go
@@ -1,8 +1,8 @@
 package iterations
 
-func Repeat(a string) (x string) {
+func Repeat(a string, y int) (x string) {
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < y; i++ {
 		x += a
 	}
 	return
--- iterations/repeat_test.go
+++ iterations/repeat_test.go
@@ -1,12 +1,21 @@
 package iterations
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("Expected %q but got %q", expected, repeated)
+	}
+
+	repeated = Repeat("a", 6)
+	expected = "aaaaaa"
 
 	if repeated != expected {
 		t.Errorf("Expected %q but got %q", expected, repeated)
@@ -16,6 +25,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(t *testing.B) {
 	for i := 0; i < t.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	expected := Repeat("a", 5)
+	fmt.Println(expected)
+	// output: aaaaa
 }
