9bd20fc91a821da9bed2e230e5161f44b194cbf2
shubham2110 <saxena.shubham.2110@gmail.com>
2021-05-29 11:59:32 +0530

    without select

--- racer/racer.go
+++ racer/racer.go
@@ -6,24 +6,17 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
 
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
-
-	if aDuration < bDuration {
+	if measureResponseTime(a) < measureResponseTime(b) {
 		return a
 	}
 
 	return b
 }
 
-// func measureResponseTime(url string) time.Duration {
-// 	startB := time.Now()
-// 	http.Get(url)
-// 	return time.Since(startB)
+func measureResponseTime(url string) time.Duration {
+	startB := time.Now()
+	http.Get(url)
+	return time.Since(startB)
 
-// }
+}
